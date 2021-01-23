d02fee46c327087eaea6300c6a0836ab2561ee82
Ben Kioko <benedictmkioko@gmail.com>
2021-01-22 19:47:37 -0500

    Allow front page to record woofs on database

--- src/index.js
+++ src/index.js
@@ -5,5 +5,25 @@ import timeline from "./timeline.js";
 const app = (() => {
     const app = document.querySelector("#app");
 
+    timeline.querySelector("button").addEventListener("click", () => {
+        const input = timeline.querySelector("#input");
+
+        console.log("Building woof...");
+
+        if (input.value.length > 0) {
+            const newWoof = {
+                text: input.value,
+                fav: 0,
+                rewoof: 0,
+            }
+            console.log("Sending woof to database...")
+
+            if (woofer.writeWoof(newWoof)) {
+                console.log("Woof has been sent!");
+                input.value = "";
+            }
+        }
+    });
+
     app.append(menu, timeline);
 })();
