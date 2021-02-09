738f677688716241873f5d674add7fe1089356b2
Ben Kioko <benedictmkioko@gmail.com>
2021-02-09 15:06:37 -0500

    Add simple login page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import login from "./login.js";
 import woofer from "./functions.js";
 import menu from "./menu.js";
 import head from "./head.js";
@@ -7,43 +8,45 @@ import reply from "./reply.js";
 const app = (() => {
     const app = document.querySelector("#app");
 
-    woofer.readWoofs(firebase.database(), woofer.renderAllWoofs);
 
-// Event listeners ----------------------------------------
+    app.appendChild(login);
+//     woofer.readWoofs(firebase.database(), woofer.renderAllWoofs);
 
-    window.addEventListener("resize", () => {
-        menu.style.left = (timeline.offsetLeft - 72) + "px";
-    });
+// // Event listeners ----------------------------------------
 
-    menu.querySelector(".fa-home").addEventListener("click", () => {
-        window.location.reload();
-    });
+//     window.addEventListener("resize", () => {
+//         menu.style.left = (timeline.offsetLeft - 72) + "px";
+//     });
 
-    head.querySelector("h1").addEventListener("click", () => {
-        window.scrollTo(0, 0);
-    });
+//     menu.querySelector(".fa-home").addEventListener("click", () => {
+//         window.location.reload();
+//     });
 
-    timeline.querySelector("button").addEventListener("click", () => {
-        const input = timeline.querySelector("#input");
+//     head.querySelector("h1").addEventListener("click", () => {
+//         window.scrollTo(0, 0);
+//     });
 
-        if (input.value.length > 0) {
-            const newWoof = {
-                text: input.value,
-                fav: 0,
-                rewoof: 0,
-            }
+//     timeline.querySelector("button").addEventListener("click", () => {
+//         const input = timeline.querySelector("#input");
 
-            if (woofer.writeWoof(newWoof)) {
-                input.value = "";
-                woofer.readWoofs(firebase.database(), woofer.renderNewWoof);
-            } else {
-                alert("Unable to send woof. Try again in a few seconds.");
-            }
-        }
-    });
+//         if (input.value.length > 0) {
+//             const newWoof = {
+//                 text: input.value,
+//                 fav: 0,
+//                 rewoof: 0,
+//             }
 
-    timeline.insertBefore(head, timeline.querySelector("#main"));
-    app.append(menu, timeline);
-    menu.style.left = (timeline.offsetLeft - 72) + "px";
+//             if (woofer.writeWoof(newWoof)) {
+//                 input.value = "";
+//                 woofer.readWoofs(firebase.database(), woofer.renderNewWoof);
+//             } else {
+//                 alert("Unable to send woof. Try again in a few seconds.");
+//             }
+//         }
+//     });
+
+//     timeline.insertBefore(head, timeline.querySelector("#main"));
+//     app.append(menu, timeline);
+//     menu.style.left = (timeline.offsetLeft - 72) + "px";
 
 })();
